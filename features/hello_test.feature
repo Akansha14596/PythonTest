20a257c5218a36a523d98ee74f9c79e37d842194
Akansha14596 <akanshadocuments@gmail.com>
2024-01-22 15:25:02 +0530

    pass array

--- features/hello_test.feature
+++ features/hello_test.feature
@@ -1,8 +1,15 @@
 Feature: Testing the Hello text on a webpage
 
-  Scenario: Check if the page contains "Hello" text 
+  Scenario: Check if the page contains following text 
     Given the user open "index.html" page
-    Then the "Hello" text should be displayed on the page
+    And I have the following items in an array:
+    """ 
+    Hello 1,Hello 2,Hello 3,
+    """
+    Then the array should be displayed on the page
+    And the "DummyData" should not be displayed on the page
     And close browser
+
+    
 
     
--- steps/stepTextTest.py
+++ steps/stepTextTest.py
@@ -7,14 +7,31 @@ from selenium.webdriver.common.by import By
 def step_open_htmlpage(context, pageName):
   dir = os.getcwd()
   context.browser = webdriver.Chrome()
+  print("Hello from html")
   context.browser.get(f'file://{dir}/{pageName}')
 
-@then('the "{text}" text should be displayed on the page')
+@given("I have the following items in an array")
+def step_fetch_array(context):
+  print(context.text);
+  context.item_list = context.text.split(',')
+  print("hello world")
+
+@then('the array should be displayed on the page')
+def step_text_shouldDisplayed(context):
+    body_text0 = context.browser.find_element(By.ID, 'id0').text
+    assert context.item_list[0] in body_text0
+    body_text1 = context.browser.find_element(By.ID, 'id1').text
+    assert context.item_list[1] in body_text1
+    body_text2 = context.browser.find_element(By.ID, 'id2').text
+    assert context.item_list[2] in body_text2
+
+@then('the "{text}" should not be displayed on the page')
 def step_text_shouldDisplayed(context,text):
-
     body_text = context.browser.find_element(By.TAG_NAME, 'body').text
-    assert text in body_text
-
+    context.text_notPresent = text not in body_text
+    print(context.text_notPresent)
+    assert context.text_notPresent is True
+	
 @then('close browser')
 def step_close_browser(context):
     context.browser.quit()
